a66ae60dc319729517a18f75f626f54f8880e244
Viktor Pyvovarov <v.s.pyvovarov@gmail.com>
2017-11-20 11:34:09 +0200

    Fix select sort

--- sort/select_sort.py
+++ sort/select_sort.py
@@ -1,8 +1,14 @@
+# 1/2 * N * N --
+# 1/2 * N * N -- for partial sorted array
+# 1/2 * N * N -- for sorted array
+# https://visualgo.net/bn/sorting
+
+
 def select_sort(array):
 
     for i in range(len(array)):
         _min = i
-        for j in range(i+1):
+        for j in range(i+1, len(array)):
             if array[_min] > array[j]:
                 _min = j
         array[i], array[_min] = array[_min], array[i]
