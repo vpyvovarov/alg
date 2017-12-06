a95700c6cf86514e932bf1d6b943eee71dd852b5
Viktor Pyvovarov <v.s.pyvovarov@gmail.com>
2017-12-06 19:50:56 +0200

    refactor heap

--- trees/heap.py
+++ trees/heap.py
@@ -1,46 +1,43 @@
 class Heap():
 
     def __init__(self):
-        self._nodes = []
-    
+        self._nodes = [None]
+
     def insert(self, value):
-        print("insert value: %s" % value)
         self._nodes.append(value)
         self.swim(len(self._nodes)-1)
-    
+
     def pop_max(self):
-        return_value = self._nodes[0]
-        self._nodes[0] = self._nodes.pop()
-        self.sink(0)
+        if self.is_empty():
+            return
+        return_value = self._nodes[1]
+        new_root = self._nodes.pop()
+        if not self.is_empty():
+            self._nodes[1] = new_root
+            self.sink(1)
         return return_value
 
     def is_empty(self):
-        return bool(len(self._nodes))
-    
+        return len(self._nodes) <= 1
+
     def swim(self, index):
-        # import pdb; pdb.set_trace()
-        if self._nodes[index] > self._nodes[index//2]:
+        while index > 1 and self._nodes[index] > self._nodes[index//2]:
             self._nodes[index], self._nodes[index//2] = self._nodes[index//2], self._nodes[index]
-            self.swim(index//2)
-    
+            index = index // 2
+
     def sink(self, index):
-        first_children_index = min([index * 2, len(self._nodes)-1])
-        second_children_index = min([first_children_index + 1, len(self._nodes)-1])
-        max_children_index = first_children_index if self._nodes[first_children_index] >= self._nodes[second_children_index] else second_children_index
+        if index * 2 > len(self._nodes)-1:
+            return
+        first_child = index * 2
+        second_child = min([first_child + 1, len(self._nodes)-1])
+        if self._nodes[first_child] >= self._nodes[second_child]:
+            max_child = first_child
+        else:
+            max_child = second_child
 
-        if self._nodes[index] < self._nodes[max_children_index]:
-            self._nodes[index], self._nodes[max_children_index] = self._nodes[max_children_index], self._nodes[index]
-            self.sink(max_children_index)
-    
+        if self._nodes[index] < self._nodes[max_child]:
+            self._nodes[index], self._nodes[max_child] = self._nodes[max_child], self._nodes[index]
+            self.sink(max_child)
+
     def __str__(self):
-        return str(self._nodes)
-
-if __name__ == "__main__":
-    h = Heap()
-    a = [2, 4, 6, 8, 2, 8, 3, 1, 9]
-    for value in a:
-        h.insert(value)
-    
-    print(h)
-    while h.is_empty():
-        print(h.pop_max())
+        return str(self._nodes[1:])
