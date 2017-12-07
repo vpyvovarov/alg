218b4b694dac6321a94a98ab7092f0a8543e9da7
Viktor Pyvovarov <v.s.pyvovarov@gmail.com>
2017-12-07 14:53:00 +0200

    Impreove heap

--- trees/heap.py
+++ trees/heap.py
@@ -7,7 +7,7 @@ class Heap():
         self._nodes.append(value)
         self.swim(len(self._nodes)-1)
 
-    def pop_max(self):
+    def pop(self):
         if self.is_empty():
             return
         return_value = self._nodes[1]
@@ -21,7 +21,8 @@ class Heap():
         return len(self._nodes) <= 1
 
     def swim(self, index):
-        while index > 1 and self._nodes[index] > self._nodes[index//2]:
+        while index > 1 and self.compare(self._nodes[index],
+                                         self._nodes[index//2]):
             self._nodes[index], self._nodes[index//2] = self._nodes[index//2], self._nodes[index]
             index = index // 2
 
@@ -30,14 +31,36 @@ class Heap():
             return
         first_child = index * 2
         second_child = min([first_child + 1, len(self._nodes)-1])
-        if self._nodes[first_child] >= self._nodes[second_child]:
+        if self.compare(self._nodes[first_child],
+                        self._nodes[second_child]):
             max_child = first_child
         else:
             max_child = second_child
 
-        if self._nodes[index] < self._nodes[max_child]:
+        if self.compare(self._nodes[max_child], self._nodes[index]):
             self._nodes[index], self._nodes[max_child] = self._nodes[max_child], self._nodes[index]
             self.sink(max_child)
 
+    def compare(self, a, b):
+        raise NotImplementedError()
+
     def __str__(self):
         return str(self._nodes[1:])
+
+    def top(self):
+        return self._nodes[1]
+
+    def size(self):
+        return len(self._nodes) - 1
+
+
+class MaxHeap(Heap):
+
+    def compare(self, a, b):
+        return a > b
+
+
+class MinHeap(Heap):
+
+    def compare(self, a, b):
+        return a < b
--- trees/test.py
+++ trees/test.py
@@ -1,20 +1,28 @@
+import pytest
 import random
 from itertools import combinations_with_replacement
-from trees.heap import Heap
+from trees.heap import MaxHeap, MinHeap
 
 
-def test():
+def _heap_sort(array, _class):
+    array = list(array)
+    sorted_array = array.copy()
+    random.shuffle(array)
+    heap = _class()
+    for item in array:
+        heap.insert(item)
+
+    result = list()
+    while not heap.is_empty():
+        result.append(heap.pop())
+    return sorted_array, result
+
+
+@pytest.mark.parametrize("_class", (MaxHeap, MinHeap))
+def test(_class):
     arrays = list(combinations_with_replacement(range(10), 10))
     for array in arrays:
-        array = list(array)
-        sorted_array = array.copy()
-        random.shuffle(array)
-        heap = Heap()
-        for item in array:
-            heap.insert(item)
-
-        result = list()
-        while not heap.is_empty():
-            result.append(heap.pop_max())
-        result.reverse()
-        assert sorted_array == result
+        sorted_array, heap_sorted = _heap_sort(array, _class)
+        if _class == MaxHeap:
+            heap_sorted.reverse()
+        assert sorted_array == heap_sorted
