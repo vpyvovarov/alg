9db4f08c034650d90177ac0f700287a3bd42cd4d
Viktor Pyvovarov <v.s.pyvovarov@gmail.com>
2017-12-27 12:41:35 +0200

    DFS

--- graph/graph.py
+++ graph/graph.py
@@ -6,6 +6,9 @@ class Graph():
     def __init__(self, num_of_v):
         self.adj = [[] for _ in range(num_of_v)]
 
+    def num_of_vert(self):
+        return len(self.adj)
+
     def add_edge(self, v1, v2):
         self.adj[v1].append(v2)
         self.adj[v2].append(v1)
@@ -27,6 +30,36 @@ class Graph():
         graph.write_png(name)
 
 
+class DFS():
+
+    def __init__(self, graph, _from):
+        self.marked = [False for _ in range(graph.num_of_vert())]
+        self.edge_to = [None for _ in range(graph.num_of_vert())]
+        self.dfs(graph, _from)
+        self._from = _from
+
+    def has_path(self, v):
+        return self.marked[v]
+
+    def path(self, _to):
+        if not self.has_path(_to):
+            return
+        path = []
+        v = _to
+        while v != self._from:
+            path.append(v)
+            v = self.edge_to[v]
+        return path
+
+    def dfs(self, graph, _from):
+
+        self.marked[_from] = True
+        for adj in graph.adjacency(_from):
+            if not self.marked[adj]:
+                self.dfs(graph, adj)
+                self.edge_to[adj] = _from
+
+
 if __name__ == "__main__":
     graph_data = [(0, 1),
                   (0, 2),
@@ -37,11 +70,14 @@ if __name__ == "__main__":
                   (6, 3),
                   (6, 1),
                   (8, 9),
-                  (8, 7),
-                  (7, 1),
-                  (9, 0)]
+                  (7, 1)]
     a = Graph(10)
     for edge in graph_data:
         a.add_edge(edge[0], edge[1])
 
     a.show_graph()
+    path = DFS(a, 0)
+    print(path.marked)
+    print(path.has_path(9))
+    print(path.has_path(3))
+    print(path.path(6))
