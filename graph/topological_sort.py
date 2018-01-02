8c97d12084120f183e278feee96c4d985d48c897
Viktor Pyvovarov <v.s.pyvovarov@gmail.com>
2018-01-02 13:37:57 +0200

    Fix topological sort

--- graph/topological_sort.py
+++ graph/topological_sort.py
@@ -19,26 +19,36 @@ class TopologicalSort():
             if not self.marked[vert]:
                 self.dfs(graph, vert)
 
+        self.stack.reverse()
         return self.stack
 
 
 if __name__ == "__main__":
-    graph_data = [(0, 5),
-                  (0, 1),
-                  (3, 5),
-                  (5, 2),
+    graph_data = [(4, 2),
+                  (2, 3),
+                  (3, 2),
                   (6, 0),
-                  (1, 4),
-                  (0, 2),
-                  (3, 6),
-                  (3, 4),
+                  (0, 1),
+                  (2, 0),
+                  (11, 12),
+                  (12, 9),
+                  (9, 10),
+                  (9, 11),
+                  (7, 9),
+                  (10, 12),
+                  (11, 4),
+                  (4, 3),
+                  (3, 5),
+                  (6, 8),
+                  (8, 6),
+                  (5, 4),
+                  (0, 5),
                   (6, 4),
-                  (3, 2)]
-    a = DiGraph(7)
+                  (6, 9),
+                  (7, 6)]
+    a = DiGraph(13)
     for edge in graph_data:
         a.add_edge(edge[0], edge[1])
-
-    a.show_graph()
-    tsorted = TopologicalSort().topological_sort(a)
+    tsorted = TopologicalSort().topological_sort(a.reverse())
     print(tsorted)
-    assert [2, 5, 4, 1, 0, 6, 3] == tsorted
+    assert [1, 0, 2, 3, 4, 11, 9, 12, 10, 6, 8, 7, 5] == tsorted
